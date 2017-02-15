b719e078e83ed7f277d25afa63ffaf0972c5c3c2
khalid <Khali.s.almuraee@gmail.com>
2017-02-15 12:12:55 +0300

    .

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
+ posts : any = [];
+  constructor(private postsService:PostsService) {}
 
   ngOnInit() {
+
+    this.postsService.getAllPosts().subscribe(posts =>{
+      this.posts = posts ;
+    })
   }
 
 }
